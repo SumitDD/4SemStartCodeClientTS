9138c8ae9bd6ba9eb361a19305e07e46ab5cb3df
Sumit Dey <sumitdey0007@gmail.com>
2021-04-27 13:21:34 +0200

    first commit

---
+++ src/components/AllFriends.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+import { useQuery, gql } from "@apollo/client"
+import ILyndaFriend from "../interfaces/interfaces"
+
+interface IFriends {
+  allFriends: ILyndaFriend[]
+}
+
+export const ALL_FRIENDS = gql`
+query{
+  allFriends {
+    id
+    firstName
+    lastName
+    role
+  }
+}
+
+`
+export default function All() {
+  const { loading, error, data, refetch } = useQuery<IFriends>(
+    ALL_FRIENDS,
+    {
+      fetchPolicy: "cache-and-network"
+    }
+  )
+
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr><th>ID</th><th>Firstname</th><th>Lastname</th><th>Email</th></tr>
+        </thead>
+        <tbody>
+          {data && data.allFriends.map(f => (
+            <tr key={f.id}><td>{f.id}</td><td>{f.firstName}</td><td>{f.lastName}</td><td>{f.email}</td></tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={() => refetch()}>Refetch</button>
+    </div >
+  )
+}
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,15 @@
+// export enum Gender {
+//   MALE,
+//   FEMALE,
+//   OTHER
+// }
+
+export type Gender = "MALE" | "FEMALE" | "OTHER"
+
+export default interface ILyndaFriend {
+  id?: string
+  firstName: string
+  lastName: string
+  password: string,
+  email: string
+}
